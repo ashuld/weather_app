6d06412d64202a4b5b90409c6bd6562f2a7d06d1
Ashish Wilson <ashish.wils@gmail.com>
2023-05-19 06:06:29 +0530

    added api

---
+++ lib/api/weatherapi.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_app/weathermodel/weathermodel.dart';
+
+class WeatherData{
+  Future<Weather> getData(var latitude, var longitude)async{
+    var uricall =Uri.parse('http://api.weatherapi.com/v1/current.json?key=eb4e07c152cc417a95c121937231705&q=$latitude,$longitude&aqi=no');
+    var response= await http.get(uricall);
+    var body= jsonDecode(response.body);
+    return Weather.fromJson(body);
+  }
+}
